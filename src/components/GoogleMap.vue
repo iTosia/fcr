fceff428f43ea2f7310aadbcaab701e7d85181dc
Anton Pomyluiko <60985834+iTosia@users.noreply.github.com>
2023-11-16 01:41:08 +0200

    created the google map component

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="google-map">
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d476.0829448839218!2d30.497364963815052!3d50.44915569396633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cf55443f5d51%3A0x46bfe658b144f64f!2z0JvQsNC30LXRgNC90LDRjyDRjdC_0LjQu9GP0YbQuNGPIENhbmRlbGE!5e0!3m2!1suk!2sua!4v1700087979868!5m2!1suk!2sua"
+        frameborder="0"
+        style="border:0"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+    >
+    </iframe>
+  </div>
+</template>
+
+<script setup>
+// import { ref } from 'vue'
+
+</script>

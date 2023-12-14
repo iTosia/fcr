87ae251551fb5a28c564013cf3e886fb0bceba63
Anton Pomyluiko <60985834+iTosia@users.noreply.github.com>
2023-12-14 11:22:53 +0200

    fixed img modal

--- src/components/ModalProduct.vue
+++ src/components/ModalProduct.vue
@@ -6,7 +6,7 @@
           <div class="modal-close" @click.stop="closeModal()"><span>&times;</span></div>
           <div class="modal-body">
             <div class="modal-product-image">
-              <img :src="`src/assets/images/${chosenProduct.image}`" :alt="`img-` + chosenProduct.id" />
+              <img :src="`${chosenProduct.image}`" :alt="`img-` + chosenProduct.id" />
             </div>
             <h3 class="modal-product-title card-title mbr-fonts-style display-2 mt-3"><strong>{{ chosenProduct.title }}</strong></h3>
             <p class="modal-product-description mbr-text mbr-fonts-style display-7 mb-0">{{ chosenProduct.description }}</p>
